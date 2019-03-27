eb41a0de08a6fcb356a1abe92170a479d746ddc0
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2019-03-27 10:55:04 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,35 +1,32 @@
 
 package multithread;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class ThreadingCls1 {
+public class Threading1 {
     public static void main(String args[])
     {
-        Arrays a = new Arrays();
+        Arrays arr = new Arrays();
         
         Subthread t = new Subthread();
         t.start();
         
         for(int i=0; i<10; i++)
         {
-            a.E[i] = a.A[i]+a.B[i];
+            arr.E[i] = arr.A[i]+arr.B[i];
         }
         try {
             t.join();
         } catch (InterruptedException ex) {
-            Logger.getLogger(ThreadingCls1.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Threading1.class.getName()).log(Level.SEVERE, null, ex);
         }
         
         for(int i=0; i<10; i++)
         {
-            a.G[i]=a.E[i]+a.F[i];
+            arr.G[i]=arr.E[i]+arr.F[i];
         }
         
         for(int i=0; i<10; i++)
         {
-            System.out.println(a.G[i]);
+            System.out.println(arr.G[i]);
         }
     }
     
